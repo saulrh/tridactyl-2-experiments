99a1d3f30b46e9fe896d81278d0721af0569330e
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2019-05-19 13:56:31 -0700

    Basic setup for project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const { TsConfigPathsPlugin } = require("awesome-typescript-loader")
+const CopyWebPackPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+
+  entry: {
+    content: "./src/content.ts",
+  },
+
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/build",
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+    plugins: [new TsConfigPathsPlugin()],
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+
+  plugins: [
+    new CopyWebPackPlugin([
+      { from: "src/manifest.json" },
+    ]),
+  ],
+}
